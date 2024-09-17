9f3dd0ba2bbe3c4d21cdf6b69285cef9c7543a71
yahia008 <yahyatijjani99@gmail.com>
2024-09-17 12:00:54 +0000

    berry

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,9 @@
+/**
+ * Prints a message to the standard output (STDOUT).
+ * @param {String} msg The message to be displayed.
+ */
+const displayMessage = (msg) => {
+  console.log(msg);
+};
+
+module.exports = displayMessage;
---
+++ 0x05-Node_JS_basic/4-http.js
@@ -0,0 +1,15 @@
+const http = require('http');
+
+const hostname = '127.0.0.1';
+const port = 1245;
+
+const app = http.createServer((request, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello Holberton School!');
+});
+
+app.listen(port, hostname, () => {
+});
+
+module.exports = app;
